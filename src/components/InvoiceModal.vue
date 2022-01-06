7bd2d62413c02aebdeb9d4783acc55095c71b8d4
Ruben Fernandes <rubenfernands@gmail.com>
2022-01-06 18:27:02 +0000

    init

---
+++ src/components/InvoiceModal.vue
@@ -0,0 +1,428 @@
+<template>
+  <div
+    @click="checkClick"
+    ref="invoiceWrap"
+    class="invoice-wrap flex flex-column"
+  >
+    <form @submit.prevent="submitForm" class="invoice-content">
+      <h1>Nova Fatura</h1>
+
+      <!-- Bill From -->
+      <div class="bill-from flex flex-column">
+        <h4>Fatura De</h4>
+        <div class="input flex flex-column">
+          <label for="billerStreetAddress">Morada</label>
+          <input
+            required
+            type="text"
+            id="billerStreetAddress"
+            v-model="billerStreetAddress"
+          />
+        </div>
+        <div class="location-details flex">
+          <div class="input flex flex-column">
+            <label for="billerCity">Cidade</label>
+            <input required type="text" id="billerCity" v-model="billerCity" />
+          </div>
+          <div class="input flex flex-column">
+            <label for="billerZipCode">Código Postal</label>
+            <input
+              required
+              type="text"
+              id="billerZipCode"
+              v-model="billerZipCode"
+            />
+          </div>
+          <div class="input flex flex-column">
+            <label for="billerCountry">País</label>
+            <input
+              required
+              type="text"
+              id="billerCountry"
+              v-model="billerCountry"
+            />
+          </div>
+        </div>
+      </div>
+
+      <!-- Bill To -->
+      <div class="bill-to flex flex-column">
+        <h4>Para</h4>
+        <div class="input flex flex-column">
+          <label for="clientName">Nome</label>
+          <input required type="text" id="clientName" v-model="clientName" />
+        </div>
+        <div class="input flex flex-column">
+          <label for="clientEmail">Email</label>
+          <input required type="text" id="clientEmail" v-model="clientEmail" />
+        </div>
+        <div class="input flex flex-column">
+          <label for="clientStreetAddress">Morada</label>
+          <input
+            required
+            type="text"
+            id="clientStreetAddress"
+            v-model="clientStreetAddress"
+          />
+        </div>
+        <div class="location-details flex">
+          <div class="input flex flex-column">
+            <label for="clientCity">Cidade</label>
+            <input required type="text" id="clientCity" v-model="clientCity" />
+          </div>
+          <div class="input flex flex-column">
+            <label for="clientZipCode">Código Postal</label>
+            <input
+              required
+              type="text"
+              id="clientZipCode"
+              v-model="clientZipCode"
+            />
+          </div>
+          <div class="input flex flex-column">
+            <label for="clientCountry">País</label>
+            <input
+              required
+              type="text"
+              id="clientCountry"
+              v-model="clientCountry"
+            />
+          </div>
+        </div>
+      </div>
+
+      <!-- Invoice Work Details -->
+      <div class="invoice-work flex flex-column">
+        <div class="payment flex">
+          <div class="input flex flex-column">
+            <label for="invoiceDate">Data Fatura</label>
+            <input
+              disabled
+              type="text"
+              id="invoiceDate"
+              v-model="invoiceDate"
+            />
+          </div>
+          <div class="input flex flex-column">
+            <label for="paymentDueDate">Data Pagamento</label>
+            <input
+              disabled
+              type="text"
+              id="paymentDueDate"
+              v-model="paymentDueDate"
+            />
+          </div>
+        </div>
+        <div class="input flex flex-column">
+          <label for="paymentTerms">Termos Pagamento</label>
+          <select required type="text" id="paymentTerms" v-model="paymentTerms">
+            <option value="30">30 dias</option>
+            <option value="60">60 dias</option>
+          </select>
+        </div>
+        <div class="input flex flex-column">
+          <label for="productDescription">Descrição Produto</label>
+          <input
+            required
+            type="text"
+            id="productDescription"
+            v-model="productDescription"
+          />
+        </div>
+        <div class="work-items">
+          <h3>Lista de produtos</h3>
+          <table class="item-list">
+            <tr class="table-heading flex">
+              <th class="item-name">Nome Produto</th>
+              <th class="qty">Qtd</th>
+              <th class="price">Preço</th>
+              <th class="total">Total</th>
+            </tr>
+            <tr
+              class="table-items flex"
+              v-for="(item, index) in invoiceItemList"
+              :key="index"
+            >
+              <td class="item-name">
+                <input type="text" v-model="item.itemName" />
+              </td>
+              <td class="qty"><input type="text" v-model="item.qty" /></td>
+              <td class="price"><input type="text" v-model="item.price" /></td>
+              <td class="total flex">
+                €{{ (item.total = item.qty * item.price) }}
+              </td>
+              <img
+                @click="deleteInvoiceItem(item.id)"
+                src="@/assets/icon-delete.svg"
+                alt=""
+              />
+            </tr>
+          </table>
+
+          <div @click="addNewInvoiceItem" class="flex button">
+            <img src="@/assets/icon-plus.svg" alt="" />
+            Novo Item
+          </div>
+        </div>
+      </div>
+
+      <!-- Save/Exit -->
+      <div class="save flex">
+        <div class="left">
+          <button @click="closeInvoice" class="red">Cancel</button>
+        </div>
+        <div class="right flex">
+          <button @click="saveDraft" class="dark-purple">
+            Guardar Rascunho
+          </button>
+          <button @click="publishInvoice" class="purple">Gerar Fatura</button>
+        </div>
+      </div>
+    </form>
+  </div>
+</template>
+
+<script>
+import { mapMutations } from "vuex";
+import { uid } from "uid";
+export default {
+  name: "InvoiceModal",
+  data() {
+    return {
+      dateOptions: { year: "numeric", month: "short", day: "numeric" },
+      billerStreetAddress: null,
+      billerCity: null,
+      billerZipCode: null,
+      billerCountry: null,
+      clientName: null,
+      clientEmail: null,
+      clientStreetAddress: null,
+      clientCity: null,
+      clientZipCode: null,
+      clientCountry: null,
+      invoiceDateUnix: null,
+      invoiceDate: null,
+      paymentTerms: null,
+      paymentDueDateUnix: null,
+      paymentDueDate: null,
+      productDescription: null,
+      invoicePending: null,
+      invoiceDraft: null,
+      invoiceItemList: [],
+      invoiceTotal: 0,
+    };
+  },
+  created() {
+    this.invoiceDateUnix = Date.now();
+    this.invoiceDate = new Date(this.invoiceDateUnix).toLocaleDateString(
+      "en-us",
+      this.dateOptions
+    );
+  },
+  methods: {
+    ...mapMutations(["TOGGLE_INVOICE"]),
+    closeInvoice() {
+      this.TOGGLE_INVOICE();
+    },
+
+    addNewInvoiceItem() {
+      this.invoiceItemList.push({
+        id: uid(),
+        itemName: "",
+        qty: "",
+        price: 0,
+        total: 0,
+      });
+    },
+
+    deleteInvoiceItem(id){
+        this.invoiceItemList = this.invoiceItemList.filter(item => item.id !== id);
+    }
+  },
+  watch: {
+    paymentTerms() {
+      const futureDate = new Date();
+      this.paymentDueDateUnix = futureDate.setDate(
+        futureDate.getDate() + parseInt(this.paymentTerms)
+      );
+      this.paymentDueDate = new Date(
+        this.paymentDueDateUnix
+      ).toLocaleDateString("en-us", this.dateOptions);
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.invoice-wrap {
+  position: fixed;
+  top: 0;
+  left: 0;
+  background-color: transparent;
+  width: 100%;
+  height: 100vh;
+  overflow: scroll;
+  &::-webkit-scrollbar {
+    display: none;
+  }
+  @media (min-width: 900px) {
+    left: 90px;
+  }
+
+  .invoice-content {
+    position: relative;
+    padding: 56px;
+    max-width: 700px;
+    width: 100%;
+    background-color: #141625;
+    color: #fff;
+    box-shadow: 10px 4px 6px -1px rgba(0, 0, 0, 0.2),
+      0 2px 4px -1px rgba(0, 0, 0, 0.6);
+
+    h1 {
+      margin-bottom: 48px;
+      color: #fff;
+    }
+
+    h3 {
+      margin-bottom: 16px;
+      font-size: 18px;
+      color: #777f98;
+    }
+
+    h4 {
+      color: #7c5dfa;
+      font-size: 12px;
+      margin-bottom: 24px;
+    }
+
+    //Bill TO / Bill FROM
+
+    .bill-to,
+    .bill-from {
+      margin-bottom: 48px;
+
+      .location-details {
+        gap: 16px;
+        div {
+          flex: 1;
+        }
+      }
+    }
+
+    //invoice Work
+
+    .invoice-work {
+      .payment {
+        gap: 24px;
+        div {
+          flex: 1;
+        }
+      }
+
+      .work-items {
+        .item-list {
+          width: 100%;
+
+          //item Table Styling
+
+          .table-heading,
+          .table-items {
+            gap: 16px;
+            font-size: 12px;
+
+            .item-name {
+              flex-basis: 50%;
+            }
+
+            .qty {
+              flex-basis: 10%;
+            }
+
+            .price {
+              flex-basis: 20%;
+            }
+
+            .total {
+              flex-basis: 20%;
+              align-self: center;
+            }
+          }
+
+          .table-heading {
+            margin-bottom: 16px;
+
+            th {
+              text-align: left;
+            }
+          }
+
+          .table-items {
+            position: relative;
+            margin-bottom: 24px;
+
+            img {
+              position: absolute;
+              top: 15px;
+              right: 0;
+              width: 12px;
+              height: 16px;
+            }
+          }
+        }
+
+        .button {
+          color: #fff;
+          background-color: #252945;
+          align-items: center;
+          justify-content: center;
+          width: 100%;
+
+          img {
+            margin-right: 4px;
+          }
+        }
+      }
+    }
+
+    .save {
+      margin-top: 60px;
+
+      div {
+        flex: 1;
+      }
+
+      button {
+        padding: 16px 22px;
+      }
+
+      .right {
+        justify-content: flex-end;
+      }
+    }
+  }
+
+  .input {
+    margin-bottom: 24px;
+  }
+
+  label {
+    font-size: 12px;
+    margin-bottom: 6px;
+  }
+
+  input,
+  select {
+    width: 100%;
+    background-color: #1e2139;
+    color: #fff;
+    border-radius: 4px;
+    padding: 12px 4px;
+    border: none;
+
+    &:focus {
+      outline: none;
+    }
+  }
+}
+</style>
---
+++ src/views/Home.vue
@@ -0,0 +1,138 @@
+<template>
+  <div class="home container">
+    <!-- HEADER -->
+    <div class="header flex">
+      <div class="left flex flex-column">
+        <h1>Faturas</h1>
+        <span>Existem 4 faturas no total</span>
+      </div>
+      <div class="right flex">
+        <div @click="toggleFilterMenu" class="filter flex">
+          <span>Filtrar por estado</span>
+          <img src="@/assets/icon-arrow-down.svg" alt="" />
+          <ul v-show="filterMenu" class="filter-menu">
+            <li>Rascunho</li>
+            <li>Pendente</li>
+            <li>Pago</li>
+            <li>Limpar Filtro</li>
+          </ul>
+        </div>
+        <div @click="newInvoice" class="button flex">
+          <div class="inner-button flex">
+            <img src="@/assets/icon-plus.svg" alt="">
+          </div>
+          <span>Nova Fatura</span>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import {mapMutations} from 'vuex';
+export default {
+  name: "Home",
+  data() {
+    return {
+        filterMenu: null,
+      };
+  },
+  components: {},
+  methods: {
+    ...mapMutations(['TOGGLE_INVOICE']),
+    newInvoice() {
+      this.TOGGLE_INVOICE();
+    },
+
+    toggleFilterMenu() {
+      this.filterMenu = !this.filterMenu;
+    }
+  }
+};
+</script>
+
+
+<style lang="scss" scoped>
+.home{
+  color: #fff;
+  .header{
+    margin-top: 65px;
+
+    .left,
+    .right{
+      flex: 1;
+
+    }
+
+    .right{
+      justify-content: flex-end;
+      align-items: center;
+
+      .button,
+      .filter{
+        align-items: center;
+
+        span{
+          font-size: 12px;
+
+        }
+      }
+      .filter{
+        cursor: pointer;
+        position: relative;
+        margin-right: 40px;
+
+        span,
+        img{
+          pointer-events: none;
+        }
+
+        img {
+          margin-left: 12px;
+          width: 9px;
+          height: 5px;
+        }
+
+        .filter-menu{
+          width: 120px;
+          position: absolute;
+          top:25px;
+          list-style: none;
+          background-color: #1e2139;
+          box-shadow: 0 4px 6px -1px rgba(0,0,0,0.1), 0 2px 4px -1px rgba(0,0,0,0.6);
+
+          li{
+            cursor: pointer;
+            font-size: 12px;
+            padding: 10px 20px;
+
+            &:hover{
+              color: #1e2139;
+              background-color: #fff;
+            }
+            }
+        }
+      }
+
+      .button{
+        padding: 8px 10px;
+        background-color: #7c5dfa;
+        border-radius: 40px;
+
+        .inner-button{
+          margin-right: 8px;
+          border-radius: 50%;
+          padding: 8px;
+          align-items: center;
+          justify-content: center;
+          background-color: #fff;
+          img{
+            width: 10px;
+            height: 10px;
+          }
+        }
+      }
+    }
+    }
+  }
+</style>
